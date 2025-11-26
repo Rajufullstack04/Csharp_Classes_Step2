cbabbf75d1fbc5537a0d1d3940a28d57b32006c6
Rajufullstack04 <rajufullstack777@gmail.com>
2025-11-26 12:08:42 +0530

    changes

---
+++ Csharp_Classes_Step2/13.Polymorphisam/2.RunTimePolymorphisam.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RuntimePolyVehicles
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Speed { get; set; }
+
+        // Virtual method
+        public virtual void Drive()
+        {
+            Console.WriteLine($"{Brand} is driving at {Speed} km/h.");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine($"{Brand} car cruises smoothly at {Speed} km/h.");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine($"{Brand} bike zips through traffic at {Speed} km/h.");
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine($"{Brand} truck hauls cargo at {Speed} km/h.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<Vehicle> vehicles = new List<Vehicle>
+            {
+                new Car { Brand = "Toyota", Speed = 80 },
+                new Bike { Brand = "Yamaha", Speed = 60 },
+                new Truck { Brand = "Volvo", Speed = 50 }
+            };
+
+            foreach (var v in vehicles)
+            {
+                v.Drive(); // Runtime polymorphism in action
+            }
+        }
+    }
+}
